1e4c41e4555f30e001a33a932ddee478d2e9e854
fa18kouki <kouki0180@outlook.jp>
2024-08-09 18:41:42 +0900

    commit with error

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def get_image():
 
     img_url = \
         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
-    #t
-    im = imread(img_url)
+
+
+        im = imread(img_url)
 
     st.image(
         im,
